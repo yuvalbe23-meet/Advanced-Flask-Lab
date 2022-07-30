f42c134bab46746e06bb03eefa6b01e0c5a3d08c
yuvalbe23-meet <109899974+yuvalbe23-meet@users.noreply.github.com>
2022-07-30 17:31:53 +0300

    done

--- Advanced-Flask-Lab/app.py
+++ Advanced-Flask-Lab/app.py
@@ -18,14 +18,14 @@ posts = {
     "https://media-exp1.licdn.com/dms/image/C4D1BAQFTbpgMk3KTSg/company-background_10000/0/1614595305396?e=1659178800&v=beta&t=OiSIvxsPJiJkArJIzBCKVF0_-yEta9gv1qLVnViU8bo": "MEET graduation!",
     "https://pbs.twimg.com/media/FPvsO6xVkAEcrBm?format=jpg&name=900x900": "#Throwback to one of our favorite #MEETsummer events: #BowlingNight!",
     "https://pbs.twimg.com/media/FI_UkcnVIAAUvWN?format=jpg&name=medium": "2020 cohort in their Y1 summer!"}
-
+hehe= "ffsd"
 
 #####
 
 
 @app.route('/')  # '/' for the default page
 def home():
-    return render_template('index.html')
+    return render_template('index.html',image_link = image_link,hehe=user_bio, posts=posts )
 
 
 @app.route('/about')  # '/' for the default page
